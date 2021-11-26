e752273d5ac6ead38046c80445f6a9486cfcdfd0
前田 <sloth19920731@gmail.com>
2021-11-26 23:21:03 +0900

    サーバレス(python)でtodoapp作成の、最低限ポスト部分(文字決め打ち)

---
+++ wasurenai/src/lambda/lambda_function.py
@@ -0,0 +1,15 @@
+import json
+import random
+import string
+
+import boto3
+
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table("ToDoTable")
+
+def lambda_handler(event, context):
+    todo_id = "".join(random.choices(string.ascii_letters + string.digits, k=12))
+    item = {"todo_id": todo_id, "todo": "こんにちはLambdaから来ました"}
+    table.put_item(Item=item)
+    response = {"statusCode": 200, "body": json.dumps(item)}
+    return response
