5ef6beca44ece1182aa31c1fb54fac108251d934
前田 <sloth19920731@gmail.com>
2021-11-27 23:51:27 +0900

    eventからAPIGatewayのpost経由のデータを取り出して動的に取得

--- wasurenai/src/lambda/lambda_function.py
+++ wasurenai/src/lambda/lambda_function.py
@@ -8,8 +8,9 @@ dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("ToDoTable")
 
 def lambda_handler(event, context):
+    todo = event["todo"]
     todo_id = "".join(random.choices(string.ascii_letters + string.digits, k=12))
-    item = {"todo_id": todo_id, "todo": "こんにちはLambdaから来ました"}
+    item = {"todo_id": todo_id, "todo": todo}
     table.put_item(Item=item)
     response = {"statusCode": 200, "body": json.dumps(item)}
     return response
