0249a5e73b7aef73e6e5dc9f2c685271984a9c8a
前田 <sloth19920731@gmail.com>
2021-12-02 23:30:03 +0900

    json.loads(event["body"])でAPIGatewayからbodyを受け取ることで、APIGateway経由でも変になるバグ解消

--- wasurenai/src/lambda/lambda_function.py
+++ wasurenai/src/lambda/lambda_function.py
@@ -8,8 +8,13 @@ dynamodb = boto3.resource("dynamodb")
 table = dynamodb.Table("ToDoTable")
 
 def lambda_handler(event, context):
-    print("eventの")
+    print("eventの値")
     print(event)
+    print("bodyの値")
+    print(event["body"])
+    body = json.loads(event["body"])
+    print("todoの値")
+    print(body["todo"])
     todo = event
     todo_id = "".join(random.choices(string.ascii_letters + string.digits, k=12))
     item = {"todo_id": todo_id, "todo": todo}
@@ -24,7 +29,8 @@ def lambda_handler(event, context):
     return  {
         'statusCode': 200,
         'headers': {
-            "Access-Control-Allow-Origin": "*"
+            "Access-Control-Allow-Headers": "*",
+            "Access-Control-Allow-Origin": "*",
         },
         'body': json.dumps(item)
     }
